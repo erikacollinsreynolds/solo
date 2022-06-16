f69ed4b031151f58aa31ffd7e935399d0bc09b28
Erika Collins <erikasusancollins@gmail.comclear>
2022-06-15 21:45:44 -0600

    wednesday inital set up

---
+++ README.md
@@ -0,0 +1,24 @@
+# soloProject
+Solo Project
+
+TO DO: 
+
+// DEV SETUP //
+[] webpack
+[] basic server
+[] eslinting (?)
+[] congif file (?)
+[] install dependencies
+[] package json
+[] install loaders?
+[] npm install
+
+// PAGES //
+[] login
+[] signup
+[] homepage
+[] add note
+[] view notes
+
+// THINGS TO LOOK INTO //
+[] package.json -- what should main be? index.js?
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    //mode: tells webpack to run either production or development
+    mode: process.env.NODE_ENV,
+
+    //entry: tells webpack where our top most level of our front end is so that can start compiling
+    entry: {
+        src: '.client/index.js'
+    },
+
+    //output: tells webpack where to output our compiled file
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/, //tests for any js/jsx files
+                exclude: /node_modules/, //exclude any files inside node modules folder
+                loader: 'babel-loader',
+                options: {
+                    //for babel-loader 8.x | babel 7.x
+                    presets: ['@babel/env', '@babel/react']
+                    //vs. ['env', 'react'] for babel-loader 7.x | babel 8.x
+                }
+            },
+            {
+                test: /\.s?css/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'index.html'
+        }),
+    ],
+
+    devServer: {
+        static: {
+            publicPath: '/build',
+            directory: path.resolve(__dirname, 'build')
+        },
+        proxy: {
+            '/api': 'http://localhost:3000'
+        }
+    }
+}
